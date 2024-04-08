37ac2f59fce6bfc146a27c70efbfed438daf874c
CamiloP121 <andresospino14@gmail.com>
2024-04-08 12:11:18 -0500

    fix and create pipeline

---
+++ fonoSemillIAS/pipeline/preproc.py
@@ -0,0 +1,35 @@
+from fonoSemillIAS.preproc.othersFilters import DWT_filter
+from fonoSemillIAS.preproc.othersFilters import nrp_filter
+from fonoSemillIAS.preproc.temporalFilters import median_filter
+
+def apply_preproc(signal, fs, torch):
+  """
+  Applies preprocessing to a signal.
+
+  Parameters:
+  - signal (array): The input signal.
+  - fs (float): The sampling frequency of the signal.
+  - torch (bool): Indicates whether to use PyTorch's adaptive threshold for the filter.
+
+  Returns:
+  - signal_pb (array): The processed signal after applying preprocessing.
+
+  Raises:
+  - Exception: If an error occurs during preprocessing.
+  """
+
+  try:
+      # Applying discrete wavelet transform (DWT) filter to the signal
+      signal_dwt, _ = DWT_filter(signal, wavelet="sym10", level=5, threshold_value=0.02)
+      
+      # Applying non-redundant noise reduction filter (NRP) to the DWT signal
+      if torch: 
+          signal_pb = nrp_filter(signal=signal_dwt, std_tresh=4.5, fs=fs, torch=True)
+      else:
+          signal_pb = nrp_filter(signal=signal_dwt, std_tresh=0.5, fs=fs, torch=False)
+  except Exception as e:
+      # Handling any exceptions that may occur during preprocessing
+      print(e)
+      raise Exception("Error in preprocessing")
+  
+  return signal_pb
