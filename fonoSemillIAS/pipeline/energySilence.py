1dee75fa6f3637bd90797ccc78ff6402d590ea1d
CamiloP121 <andresospino14@gmail.com>
2024-04-08 19:27:53 -0500

    fix name variable

--- fonoSemillIAS/pipeline/energySilence.py
+++ fonoSemillIAS/pipeline/energySilence.py
@@ -26,39 +26,39 @@ def apply_energy_silences(signal, fs, window_ms=5, output="intervals"):
 
         # Step 1: Energy signal
         tqdm.write("Start step 1: Energy signal")
-        energy_wave = shannon_energy(signal=signal)
+        energy_wave = shannon_energy(signal = signal)
         # Update progress bar
         progress_bar.update(1)
 
         # Step 2: Average energy
         tqdm.write("Start step 2: Average energy")
         window = window_ms * 0.001
-        avr_energy = avr_energy(energy_sequence=energy_wave, window_move=int(window * fs))
+        average_energy = avr_energy(energy_sequence = energy_wave, window_move = int(window * fs))
         # Update progress bar
         progress_bar.update(1)
 
         # Step 3: Envelogram
         tqdm.write("Start step 3: Envelogram")
-        envelogram = envelogram(average_energy_sequence=avr_energy)
+        envelogram = envelogram(average_energy_sequence = average_energy)
         # Update progress bar
         progress_bar.update(1)
 
         # Step 4: Detection of silences
         ## Sub-step 4.1: Zero crossing
         tqdm.write("Start step 4.1: Zero crossing")
-        zero_crossing = find_zero_crossings(envelogram=envelogram)
+        zero_crossing = find_zero_crossings(envelogram = envelogram)
         # Update progress bar
         progress_bar.update(1)
 
         ## Sub-step 4.2: Identify lobes
         tqdm.write("Start step 4.2: Identify lobes")
-        lobe_indices = identify_lobes(envelogram=envelogram, zero_crossings=zero_crossing)
+        lobe_indices = identify_lobes(envelogram = envelogram, zero_crossings = zero_crossing)
         # Update progress bar
         progress_bar.update(1)
 
         ## Sub-step 4.3: Transform silence lobes into pulse
         tqdm.write("Start step 4.3: Transform silence lobes into pulse")
-        intervals, pulse = silence_pulse(envelogram=envelogram, lobe_indices=lobe_indices)
+        intervals, pulse = silence_pulse(envelogram = envelogram, lobe_indices = lobe_indices)
         # Update progress bar
         progress_bar.update(1)
 
