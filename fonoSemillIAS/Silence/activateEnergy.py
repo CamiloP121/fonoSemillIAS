039079cf76af045013f337e22a2b666286b0a8c0
CamiloP121 <andresospino14@gmail.com>
2024-04-08 19:15:56 -0500

    add detect silence funtions and pipeline

---
+++ fonoSemillIAS/Silence/activateEnergy.py
@@ -0,0 +1,114 @@
+import numpy as np
+
+def shannon_energy(signal):
+    """
+    Calculates the Shannon energy of an audio signal.
+
+    Parameters:
+    - signal (array): The audio signal.
+
+    Returns:
+    - energy (float): The Shannon energy of the signal.
+    """
+    energy = -(signal**2) * np.log10(signal**2)
+    return np.nan_to_num(energy)
+
+def avr_energy(energy_sequence, window_move):
+    """
+    Calculates the continuous average energy for every 50 ms along the original signal.
+
+    Parameters:
+    - energy_sequence (array): The energy sequence of the original signal.
+    - window_move (int): Size of the moving window in ms.
+    - overlap (float): Overlap percentage between consecutive windows.
+
+    Returns:
+    - average_energy (array): The calculated continuous average energy.
+    """
+    # Calculate the number of samples that overlap between consecutive windows
+    print(window_move)
+    window_move = window_move // 2
+    print(window_move)
+    # Initialize the index to start the window
+    start_index = 0
+    average_energy, t = [], []
+    # Loop through the signal with the specified overlap
+    while start_index < len(energy_sequence):
+        # Take the energy over the window
+        window_energy = energy_sequence[start_index - window_move : start_index + window_move]
+        # Calculate the average energy over the window
+        average_energy.append(np.mean(window_energy))
+        # Move the window with the overlap
+        start_index += 1
+    return np.nan_to_num(np.array(average_energy))
+
+def envelogram(average_energy_sequence):
+    """
+    Calculates the normalized envelogram.
+
+    Parameters:
+    - average_energy_sequence (array): The sequence of continuous average energy.
+
+    Returns:
+    - envelogram (array): The calculated normalized envelogram.
+    """
+    mean = np.mean(average_energy_sequence)
+    standard_deviation = np.std(average_energy_sequence)
+    print(mean, standard_deviation)
+    return (average_energy_sequence - mean) / standard_deviation
+
+def find_zero_crossings(envelogram):
+    """
+    Find the indices of zero crossings in the envelogram.
+
+    Parameters:
+    - envelogram (array): The envelogram of the signal.
+
+    Returns:
+    - zero_crossings (array): Indices of zero crossings.
+    """
+    zero_crossings = np.where(np.diff(np.sign(envelogram)))[0]
+    return zero_crossings
+
+def identify_lobes(envelogram, zero_crossings):
+    """
+    Identify the lobes in the envelogram using zero crossings.
+
+    Parameters:
+    - envelogram (array): The envelogram of the signal.
+    - zero_crossings (array): Indices of zero crossings.
+
+    Returns:
+    - lobe_indices (list of tuples): Indices of the lobes.
+    """
+    lobe_indices = []
+    start_idx = 0
+    for zero_crossing in zero_crossings:
+        lobe_indices.append((start_idx, zero_crossing))
+        start_idx = zero_crossing
+    # Add the last lobe
+    lobe_indices.append((start_idx, len(envelogram) - 1))
+    return lobe_indices
+
+def silence_pulse(envelogram, lobe_indices):
+    """
+    Identify and mark regions of silence in an envelogram.
+
+    Parameters:
+    - envelogram (array): Envelogram of the signal.
+    - lobe_indices (list of tuples): Indices of the lobes in the envelogram.
+
+    Returns:
+    - intervals (list of tuples): List of tuples (i, j) where the mean value of the lobe is negative.
+    - pulse (array): Array marking the regions of silence with 1.
+    """
+    pulse = np.zeros_like(envelogram)
+    intervals = []
+
+    for start_index, final_index in lobe_indices:
+        arr = envelogram[start_index:final_index]
+        if np.mean(arr) < 0:
+            intervals.append((start_index, final_index))
+            pulse[start_index:final_index] = 1
+
+    return intervals, pulse
---
+++ fonoSemillIAS/pipeline/energySilence.py
@@ -0,0 +1,64 @@
+from tqdm import tqdm
+from fonoSemillIAS.Silence.activateEnergy import *
+
+def apply_energy_silences(signal, fs, window_ms = 5, output = "intervals"):
+
+    assert output in ["intervals", "all"], "Parameter 'output' is only intervals or all"
+
+    progress_bar = tqdm(total=6, desc='Progress', position=0)
+
+    # 1. Energy signal 
+    tqdm.write("Start step 1: Energy signal")
+    energy_wave = shannon_energy(signal=signal)
+    # Update progress bar
+    progress_bar.update(1)
+
+    # 2. Average energy
+    tqdm.write("Start step 2: Average energy")
+    window = window_ms * 0.001
+    avr_energy = avr_energy(energy_sequence=energy_wave, window_move=int(window * fs))
+    # Update progress bar
+    progress_bar.update(1)
+
+    # 3. envelogram
+    tqdm.write("Start step 3: Envelogram")
+    envelogram = envelogram(average_energy_sequence=avr_energy)
+    # Update progress bar
+    progress_bar.update(1)
+
+    # 4. Detection of silences
+    ## 4.1 zero_crossing
+    tqdm.write("Start step 4.1: Zero crossing")
+    zero_crossing = find_zero_crossings(envelogram=envelogram)
+    # Update progress bar
+    progress_bar.update(1)
+
+    ## 4.2 identify lobes
+    tqdm.write("Start step 4.2: Identify lobes")
+    lobe_indices = identify_lobes(envelogram=envelogram, zero_crossings=zero_crossing)
+    # Update progress bar
+    progress_bar.update(1)
+
+    ## 4.3 Transform silence lobes in pulse
+    tqdm.write("Start step 4.3: Transform silence lobes in pulse")
+    intervals, pulse = silence_pulse(envelogram=envelogram, lobe_indices=lobe_indices)
+    # Update progress bar
+    progress_bar.update(1)
+
+    # Close progress bar
+    progress_bar.close()
+
+    if output == "intervals":
+        return {
+            "intervals_silence": intervals
+        }
+    elif output == "all":
+        return {
+            "intervals_silence": intervals,
+            "pulse": pulse,
+            "envelogram": envelogram,
+            "intervals_all": lobe_indices
+        }
+
+    
+
