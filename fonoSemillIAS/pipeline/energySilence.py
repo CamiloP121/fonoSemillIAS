1eba6aff7b10c985fa025b6eb7175a8b94db5dbe
CamiloP121 <andresospino14@gmail.com>
2024-04-08 19:22:49 -0500

    add documentation

--- fonoSemillIAS/pipeline/energySilence.py
+++ fonoSemillIAS/pipeline/energySilence.py
@@ -1,48 +1,63 @@
 from tqdm import tqdm
 from fonoSemillIAS.Silence.activateEnergy import *
 
-def apply_energy_silences(signal, fs, window_ms = 5, output = "intervals"):
+def apply_energy_silences(signal, fs, window_ms=5, output="intervals"):
+    """
+    Applies energy-based silences detection algorithm to a given signal.
 
-    assert output in ["intervals", "all"], "Parameter 'output' is only intervals or all"
+    Parameters:
+    - signal (array): The input signal.
+    - fs (float): The sampling frequency of the signal.
+    - window_ms (int): Size of the moving window for average energy calculation in milliseconds (default is 5 ms).
+    - output (str): Specifies the output format. Can be "intervals" to return only the intervals of silence,
+                    or "all" to return additional information including the pulse, envelogram, and all intervals.
 
-    try: 
+    Returns:
+    - result (dict): Dictionary containing the output based on the specified format:
+                     - If output="intervals", returns {"intervals_silence": intervals}.
+                     - If output="all", returns {"intervals_silence": intervals, "pulse": pulse,
+                                                  "envelogram": envelogram, "intervals_all": lobe_indices}.
+    """
+    assert output in ["intervals", "all"], "Parameter 'output' must be 'intervals' or 'all'"
 
+    try:
+        # Initialize progress bar
         progress_bar = tqdm(total=6, desc='Progress', position=0)
 
-        # 1. Energy signal 
+        # Step 1: Energy signal
         tqdm.write("Start step 1: Energy signal")
         energy_wave = shannon_energy(signal=signal)
         # Update progress bar
         progress_bar.update(1)
 
-        # 2. Average energy
+        # Step 2: Average energy
         tqdm.write("Start step 2: Average energy")
         window = window_ms * 0.001
         avr_energy = avr_energy(energy_sequence=energy_wave, window_move=int(window * fs))
         # Update progress bar
         progress_bar.update(1)
 
-        # 3. envelogram
+        # Step 3: Envelogram
         tqdm.write("Start step 3: Envelogram")
         envelogram = envelogram(average_energy_sequence=avr_energy)
         # Update progress bar
         progress_bar.update(1)
 
-        # 4. Detection of silences
-        ## 4.1 zero_crossing
+        # Step 4: Detection of silences
+        ## Sub-step 4.1: Zero crossing
         tqdm.write("Start step 4.1: Zero crossing")
         zero_crossing = find_zero_crossings(envelogram=envelogram)
         # Update progress bar
         progress_bar.update(1)
 
-        ## 4.2 identify lobes
+        ## Sub-step 4.2: Identify lobes
         tqdm.write("Start step 4.2: Identify lobes")
         lobe_indices = identify_lobes(envelogram=envelogram, zero_crossings=zero_crossing)
         # Update progress bar
         progress_bar.update(1)
 
-        ## 4.3 Transform silence lobes in pulse
-        tqdm.write("Start step 4.3: Transform silence lobes in pulse")
+        ## Sub-step 4.3: Transform silence lobes into pulse
+        tqdm.write("Start step 4.3: Transform silence lobes into pulse")
         intervals, pulse = silence_pulse(envelogram=envelogram, lobe_indices=lobe_indices)
         # Update progress bar
         progress_bar.update(1)
@@ -50,20 +65,18 @@ def apply_energy_silences(signal, fs, window_ms = 5, output = "intervals"):
         # Close progress bar
         progress_bar.close()
 
+        # Return output based on specified format
         if output == "intervals":
-            return {
-                "intervals_silence": intervals
-            }
+            return {"intervals_silence": intervals}
         elif output == "all":
-            return {
-                "intervals_silence": intervals,
-                "pulse": pulse,
-                "envelogram": envelogram,
-                "intervals_all": lobe_indices
-            }
+            return {"intervals_silence": intervals,
+                    "pulse": pulse,
+                    "envelogram": envelogram,
+                    "intervals_all": lobe_indices}
+
     except Exception as e:
         print(e)
-        raise Exception("Error in detec silences")
+        raise Exception("Error in silence detection")
 
     
 
