2adf75c5973723671af7667a7580e149a66583ec
CamiloP121 <andresospino14@gmail.com>
2024-04-08 19:18:51 -0500

    update progress bar and managment of raises

--- fonoSemillIAS/pipeline/preproc.py
+++ fonoSemillIAS/pipeline/preproc.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 from fonoSemillIAS.preproc.othersFilters import DWT_filter
 from fonoSemillIAS.preproc.othersFilters import nrp_filter
 from fonoSemillIAS.preproc.temporalFilters import median_filter
@@ -19,17 +21,26 @@ def apply_preproc(signal, fs, torch):
   """
 
   try:
-      # Applying discrete wavelet transform (DWT) filter to the signal
-      signal_dwt, _ = DWT_filter(signal, wavelet="sym10", level=5, threshold_value=0.02)
-      
-      # Applying non-redundant noise reduction filter (NRP) to the DWT signal
-      if torch: 
-          signal_pb = nrp_filter(signal=signal_dwt, std_tresh=4.5, fs=fs, torch=True)
-      else:
-          signal_pb = nrp_filter(signal=signal_dwt, std_tresh=0.5, fs=fs, torch=False)
+    progress_bar = tqdm(total=2, desc='Progress', position=0)
+    # 1. DWT filter
+    tqdm.write("Start step 1: DWT filter")
+    signal_dwt, _ = DWT_filter(signal, wavelet="sym10", level=5, threshold_value=0.02)
+    # Update progress bar
+    progress_bar.update(1)
+
+    # 2. Applying non-redundant noise reduction filter (NRP) to the DWT signal
+    tqdm.write("Start step 2: Applying NRP filter")
+    if torch:
+        signal_pb = nrp_filter(signal=signal_dwt, std_tresh=4.5, fs=fs, torch=True)
+    else:
+        signal_pb = nrp_filter(signal=signal_dwt, std_tresh=0.5, fs=fs, torch=False)
+    # Update progress bar
+    progress_bar.update(1)
+
+    # Close progress bar
+    progress_bar.close()
   except Exception as e:
-      # Handling any exceptions that may occur during preprocessing
-      print(e)
-      raise Exception("Error in preprocessing")
+    print(e)
+    raise Exception("Error in preprocessing")
   
   return signal_pb
