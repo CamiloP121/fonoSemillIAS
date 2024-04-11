997fc86393fb9a2f0bd6f57bd9bd53ea27643223
CamiloP121 <andresospino14@gmail.com>
2024-04-10 22:27:29 -0500

    add metric. delta_lobes

--- fonoSemillIAS/others/metrics.py
+++ fonoSemillIAS/others/metrics.py
@@ -14,3 +14,39 @@ def dice_coefficient(y_true, y_pred):
     """
     intersection = np.sum(y_true * y_pred)
     return (2. * intersection) / (np.sum(y_true) + np.sum(y_pred))
+
+def deltas_lobes(df_tag_silences, data_detec_silence):
+    delta_start = []
+    for i in range(len(df_tag_silences)):
+        tg = df_tag_silences.iloc[i]
+
+        for j in range(len(data_detec_silence)):
+            dect = data_detec_silence.iloc[j]
+            if tg.start_sample <= dect.start_sample and dect.start_sample <= tg.end_sample:
+                delta_start.append(  dect.start_time - tg.start_time )
+            elif dect.start_sample <= tg.start_sample and tg.start_sample <= dect.end_sample:
+                delta_start.append(  tg.start_time - dect.start_time )
+            elif dect.start_sample > tg.end_sample: 
+                break
+
+        if len(delta_start) < i + 1: 
+            delta_start.append("inf")
+
+    delta_end = []
+    for i in range(len(df_tag_silences)):
+        tg = df_tag_silences.iloc[i]
+
+        for j in range(len(data_detec_silence)):
+            dect = data_detec_silence.iloc[j]
+            if tg.start_sample <= dect.end_sample and dect.end_sample <= tg.end_sample:
+                delta_end.append(  tg.end_sample - dect.end_sample )
+            elif dect.start_sample <= tg.end_sample and tg.end_sample <= dect.end_sample:
+                delta_end.append(  dect.end_sample - tg.end_sample  )
+            elif dect.start_sample > tg.end_sample: 
+                break
+    if len(delta_end) < i + 1: 
+        delta_end.append("inf")
+
+    return delta_start, delta_end
+
+    
